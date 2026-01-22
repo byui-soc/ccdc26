916d32de8f900de8fb612e21b3867e7d90abd283
thvl3 <ethanhulse.work@gmail.com>
2026-01-21 22:55:14 -0700

    deb splunk

--- linux-scripts/utils/installsplunk.sh
+++ linux-scripts/utils/installsplunk.sh
@@ -2,16 +2,7 @@
 
 # Install Splunk Universal Forwarder
 
-# Check if it's partially installed
-ls -la /opt/splunkforwarder/bin/splunk
-
-# If missing, try manual installation
 cd /tmp
-wget https://download.splunk.com/products/universalforwarder/releases/9.1.2/linux/splunkforwarder-9.1.2-Linux-x86_64.tgz
-tar -xzf splunkforwarder-9.1.2-Linux-x86_64.tgz -C /opt/
-
-# Verify binary exists
-ls -la /opt/splunkforwarder/bin/splunk
-
-# If it exists, configure and start
-/opt/splunkforwarder/bin/splunk start --accept-license --answer-yes --no-prompt
+wget https://download.splunk.com/products/universalforwarder/releases/10.2.0/linux/splunkforwarder-10.2.0-d749cb17ea65-linux-amd64.tgz
+tar -xzf splunkforwarder-10.2.0-d749cb17ea65-linux-amd64.tgz -C /opt/
+ls -la /opt/splunkforwarder/bin/splunk  # Verify it exists
